dc135d1d596175165fb5afe668f1643267c8d227
Marius Dragan <Marius@Mariuss-MacBook-Pro.local>
2017-12-10 20:00:30 +0000

    Finished MainVC UI

--- iDeliver/iDeliver/Controller/MainVC.swift
+++ iDeliver/iDeliver/Controller/MainVC.swift
@@ -30,7 +30,9 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
        let cell = tableView.dequeueReusableCell(withIdentifier: "deliveryAddressCell", for: indexPath) as! AddressCell
         return cell
     }
-    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 165
+    }
 
 
 }
