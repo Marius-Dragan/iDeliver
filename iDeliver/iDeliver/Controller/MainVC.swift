01686f706fcebc942b8b03b75d028370fd7544aa
Marius Dragan <Marius@Mariuss-MacBook-Pro.local>
2017-12-10 17:22:56 +0000

    Creating UI

--- iDeliver/iDeliver/Controller/MainVC.swift
+++ iDeliver/iDeliver/Controller/MainVC.swift
@@ -8,17 +8,29 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var deliveryAddress: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        deliveryAddress.delegate = self
+        deliveryAddress.dataSource = self
+        
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier: "deliveryAddressCell", for: indexPath) as! AddressCell
+        return cell
+    }
+    
 
 
 }
